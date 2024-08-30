6ca68c091d54abd7d039146417dfd93519b6d132
Full stack web | blockchain developer <blackeagle0188@gmail.com>
2024-08-30 05:33:36 -0400

    Update mobile responsive for footer menu

--- assets/animations.js
+++ assets/animations.js
@@ -144,3 +144,22 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   });
 });
+
+
+//accordion
+var coll = document.getElementsByClassName("footer-block__heading");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    if (window.innerWidth < 768) {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+      }
+    }
+  });
+}
